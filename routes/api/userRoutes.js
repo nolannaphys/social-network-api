8c08180b48812fb02a904a0d93ee4971f3c331d3
Nolan <nolannaphys@gmail.com>
2023-07-31 12:54:36 -0400

    added thoughtRoutes, and updated api routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,11 +1,25 @@
 const router = require('express').Router();
 const {
  getUsers,
- createUser
+ createUser,
+ getSingleUser,
+ updateUser,
+ deleteUser,
+ addFriend,
+ removeFriend,
 } = require('../../controllers/userController');
 
 // /api/users
 router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends
+router.route('/:userId/friends').post(addFriend);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').delete(removeFriend);
 
 
 module.exports = router;
